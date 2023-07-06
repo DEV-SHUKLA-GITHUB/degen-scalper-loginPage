1932247c9c73b49742c63be23804aae0f99f4d73
Yadvendra Sharma <yadvendras20@gmail.com>
2023-07-06 14:00:26 +0530

    added routes folder

--- loginpage/backend/index.js
+++ loginpage/backend/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const brokerValidator = require("./validateBrokerCreds")
-const User = require("./userDetails"); // Import the user schema from userDetails.js
+const registerRoute=require("./routes/register")
+const loginRouter=require("./routes/login")
+const User = require("./models/userDetails"); // Import the user schema from userDetails.js
 app.use(express.json());
 const shortid = require("shortid"); 
 const cors = require("cors");
@@ -11,6 +13,9 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = "slkdfjlasdfkajsdlkfaksdflaksdjfoajsdofjodsf";
 
+
+
+//connecting to database
 const mongoUrl = "mongodb+srv://devshuklaji6:fU6D8Wu5BDaQlgEB@cluster0.mxffi4n.mongodb.net/?retryWrites=true&w=majority";
 mongoose
   .connect(mongoUrl, {
@@ -20,6 +25,13 @@ mongoose
     console.log("Connected to the database");
   })
   .catch((e) => console.log(e));
+
+
+
+  //Routes
+  app.use("/register", registerRoute)
+  app.use("/login-user", loginRouter)
+
   app.post("/check", async (req, res) => {
     const { email, Username } = req.body;
     try {
@@ -30,48 +42,7 @@ mongoose
       res.status(500).json({ error: "Error checking email and username" }); // Return the error response as JSON
     }
   });
-  app.post("/login-user", async (req, res) => {
-    const { email, password } = req.body;
   
-    // Validate user credentials
-    try {
-      // Find the user by email in your database
-      const user = await User.findOne({ email });
-  
-      // If user does not exist, send an error response
-      if (!user) {
-        return res.status(400).json({ error: "Invalid credentials" });
-      }
-  
-      // Compare the password provided with the hashed password stored in the database
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-  
-      // If password is invalid, send an error response
-      if (!isPasswordValid) {
-        return res.status(400).json({ error: "Invalid credentials" });
-      }
-  
-      // Generate a JSON Web Token (JWT) for authentication
-      const token = jwt.sign({ email: user.email }, JWT_SECRET);
-  
-      // Send the token as a response
-      res.json({ status: "ok", data: token });
-    } catch (error) {
-      console.error("Login failed:", error);
-      res.status(500).json({ error: "An error occurred during login" });
-    }
-  });
-  // app.post("/getUserData",(req, res) => {
-  //   try{
-  //     const user = jwt.verify(token, JWT_SECRET);
-  //     const userData=user.findOne({})
-  //   }
-  //   catch(e){
-  //     console.log(e)
-  //   }
-  // }
-  // )
-
   app.post("/userData", async (req, res) => {
     const { token, BrokerList } = req.body;
     console.log(BrokerList)
@@ -114,33 +85,7 @@ if(BrokerList){
   
   
   
-app.post("/register", async (req, res) => {
-  const { FullName, email, password, Username } = req.body;
-  const encrypted = await bcrypt.hash(password, 10);
-  try {
-    const emailExists = await User.exists({ email });
-    const usernameExists = await User.exists({ Username });
 
-    if (emailExists) {
-      return res.status(400).json({ error: "Email already exists" });
-    }
-
-    if (usernameExists) {
-      return res.status(400).json({ error: "Username already exists" });
-    }
-
-    await User.create({
-      FullName,
-      email,
-      password: encrypted,
-      Username,
-    });
-
-    res.send({ status: "ok" });
-  } catch (error) {
-    res.status(500).send({ error: "Registration failed" });
-  }
-});
 
 // Rest of the code remains the same
 
