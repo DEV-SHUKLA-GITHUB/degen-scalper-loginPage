4c9c66ed93173ee2342f7e88f3763810935fd900
unknown <devshuklaji6@gmail.com>
2023-07-06 13:58:26 +0530

    verifcation done

--- loginpage/backend/index.js
+++ loginpage/backend/index.js
@@ -72,20 +72,8 @@ mongoose
   // }
   // )
 
-  app.post("/userData", async (req, res) => {
-    const { token, BrokerList } = req.body;
-    console.log(BrokerList)
-if(BrokerList){
-  const obj={
-    broker_user_id : BrokerList.userId,
-    broker_user_password : BrokerList.password,
-    api_key : BrokerList.apiKey,
-    api_secret : BrokerList.secretKey,
-    totp_token : BrokerList.totp,
-    redirect_url : "http://localhost:8000",
-    broker_name: BrokerList.broker}
-    brokerValidator(obj)
-}
+  app.post("/checkAuth", async (req, res) => {
+    const { token} = req.body;
 
   
     try {
@@ -94,10 +82,8 @@ if(BrokerList){
       const userEmail = user.email;
   
       // Find the user in the database using the email
-      User.findOneAndUpdate(
-        { email: userEmail }, // Find the user by email
-        { $push: { BrokerList } }, // Push the form data to the formData array
-        { new: true } // Return the updated document
+      User.findOne(
+        { email: userEmail }
       )
         .then((userData) => {
           res.json({ status: "ok", data: userData });
@@ -105,6 +91,49 @@ if(BrokerList){
         .catch((error) => {
           res.status(500).json({ status: "error", data: error });
         });
+    } catch (error) {
+      res.status(500).json({ status: "error", data: error });
+    }
+  });
+  
+  app.post("/userData", async (req, res) => {
+    
+    var isValidated=false;
+    const { token, BrokerList } = req.body;
+    
+  const obj={
+    broker_user_id : BrokerList.userId,
+    broker_user_password : BrokerList.password,
+    api_key : BrokerList.apiKey,
+    api_secret : BrokerList.secretKey,
+    totp_token : BrokerList.totp,
+    redirect_url : "http://localhost:8000",
+    broker_name: BrokerList.broker}
+    const response=await brokerValidator(obj)
+    if(response){isValidated=true}
+
+
+
+  
+    try {
+      // Verify the token and extract the user's email
+      const user = jwt.verify(token, JWT_SECRET);
+      const userEmail = user.email;
+      // Find the user in the database using the email
+      if(isValidated){
+        User.findOneAndUpdate(
+          { email: userEmail }, // Find the user by email
+          { $push: { BrokerList } }, // Push the form data to the formData array
+          { new: true } // Return the updated document
+        )
+          .then((userData) => {
+            console.log("updated")
+            res.json({ status: "ok", data: userData });
+          })
+          .catch((error) => {
+            res.status(500).json({ status: "error", data: error });
+          });
+      }
     } catch (error) {
       res.status(500).json({ status: "error", data: error });
     }
