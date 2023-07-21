7f76204d3700e2a0d6f50025f8dd1dc949e3ba4a
unknown <devshuklaji6@gmail.com>
2023-07-21 14:30:16 +0530

    made expiry and strike faster

--- loginpage/backend/routes/instruments.js
+++ loginpage/backend/routes/instruments.js
@@ -1,6 +1,7 @@
 const KiteTicker = require("kiteconnect").KiteTicker;
 const KiteConnect = require("kiteconnect").KiteConnect;
 const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const app = express();
 const server = require("http").createServer(app);
@@ -9,8 +10,51 @@ const router = express.Router();
 const User = require("../models/userDetails"); // Import the user schema from userDetails.js
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const axios = require('axios');
+const csvtojson = require('csvtojson');
+const cron = require('node-cron');
 const JWT_SECRET = "slkdfjlasdfkajsdlkfaksdflaksdjfoajsdofjodsf";
 let instoken;
+let instrumentsData=[];
+
+// Create a directory 'data' to store the instrument.json file
+const dataDir = path.join(__dirname, 'data');
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir);
+}
+
+// Function to download the file, convert to JSON, and store it in instrument.json
+const downloadInstrumentsData = async () => {
+  try {
+    const url = 'https://api.kite.trade/instruments';
+    const response = await axios.get(url);
+    const jsonData = await csvtojson().fromString(response.data);
+
+    // Store the instruments data in instrument.json
+    fs.writeFileSync(path.join(dataDir, 'instrument.json'), JSON.stringify(jsonData, null, 2));
+    console.log('Instruments data downloaded and stored in instrument.json.');
+  } catch (error) {
+    console.error('Error downloading or converting instruments data:', error);
+  }
+};
+
+// Read instruments data from instrument.json on server start
+const instrumentDataPath = path.join(dataDir, 'instrument.json');
+if (fs.existsSync(instrumentDataPath)) {
+  const jsonData = fs.readFileSync(instrumentDataPath, 'utf-8');
+  instrumentsData = JSON.parse(jsonData).map(instrument => ({
+    ...instrument,
+    expiry: new Date(instrument.expiry), // Convert the expiry date to a JavaScript Date object
+  }));
+  console.log('Instruments data loaded from instrument.json.');
+} else {
+  console.log('Instruments data not found. Downloading...');
+  downloadInstrumentsData();
+}
+
+// Set up a cron job to download the file once every day (adjust the cron schedule as needed)
+cron.schedule('0 0 * * *', downloadInstrumentsData); // This runs the function at midnight every day
+
 router.post("/getInstruments", async (req, res) => {
   const { selected, token } = req.body;
   try {
@@ -21,7 +65,7 @@ router.post("/getInstruments", async (req, res) => {
 
     // Extract the access token
     const { accessToken, apiKey } = jsonData.BrokerList.find(broker => broker.broker === "Zerodha");
-    const access_token = accessToken; 
+    const access_token = accessToken;
     const api_key = apiKey;
 
     const a = async () => {
@@ -30,16 +74,13 @@ router.post("/getInstruments", async (req, res) => {
 
       // Perform any kite operations here
       const instruments = await kite.getInstruments(["NFO"]);    
-      const filteredInstruments = instruments.filter(
-        (instrument) => instrument.name === (selected.name || selected ) && instrument.segment === 'NFO-OPT',
-      // instoken = selected.name
-    // } 
-      
+      const filteredInstruments = instrumentsData.filter(
+        (instrument) => instrument.name === (selected.name || selected) && instrument.segment === 'NFO-OPT'
       );
 
       const uniqueExpiryDates = Array.from(
         new Set(
-          filteredInstruments.map((instrument) => instrument.expiry.getTime())
+          filteredInstruments.map((instrument) => new Date(instrument.expiry).getTime())
         )
       ).map((timestamp) => new Date(timestamp));
 
@@ -48,11 +89,9 @@ router.post("/getInstruments", async (req, res) => {
           filteredInstruments.map((instrument) => instrument.strike)
         )
       );
-      // console.log(uniqueStrikes,"unique")
-      // console.log(uniqueExpiryDates,"unique")
 
       // Send the response to the client
-      res.send({ uniqueExpiryDates, instruments,uniqueStrikes });
+      res.send({ uniqueExpiryDates, instruments, uniqueStrikes });
     };
 
     a();
@@ -72,52 +111,53 @@ wss.on('connection', (ws) => {
 
   ws.on('message', async (message) => {
     const initialData = JSON.parse(message);
-    const { token, instrumentToken, email } = initialData;   
-  
+    const { token, instrumentToken, email } = initialData;
+
     // Use the received data (token, instrumentToken, email) for further processing or to retrieve the required tick data
     try {
       const user = jwt.verify(token, JWT_SECRET);
       const { email } = user;
-      console.log(email);
+      // console.log(email);
       const jsonData = await User.findOne({ email });
-  
+
       // Extract the access token
       const { accessToken, apiKey } = jsonData.BrokerList.find(broker => broker.broker === "Zerodha");
       const access_token = accessToken;
       const api_key = apiKey;
-  
+
       const a = async () => {
         const ticker = new KiteTicker({ api_key, access_token });
-  
+
         function onTicks(ticks) {
-          console.log("Ticks", ticks);
+          // console.log("Ticks", ticks);
           const instrumentTokens = clientInstrumentMap.get(ws);
           if (instrumentTokens && instrumentTokens.includes(ticks[0].instrument_token)) {
             // Send the ticks data to the current client
-            console.log("got it")
+            // console.log("got it")
             ws.send(JSON.stringify(ticks));
           }
         }
-  
+
         function subscribe() {
           var items = [Number(instrumentToken)];
           ticker.subscribe(items)
           instoken = ticker.subscribe(items);
-          console.log(ticker.subscribe(items),"hello")
+          console.log(ticker.subscribe(items), "hello")
 
           ticker.setMode(ticker.modeQuote, items);
         }
-  
+
         function unsubscribe() {
           let instrumentTokens = [];
           const existingInstrumentTokens = clientInstrumentMap.get(ws);
-          console.log(instoken)
+          console.log(existingInstrumentTokens,"up")
+          console.log(instoken,"instoken")
           if (instoken) {
             // instrumentTokens = existingInstrumentTokens;
             ticker.unsubscribe(instoken);
           }
         }
-  
+
         ticker.connect();
         ticker.on("connect", () => {
           unsubscribe(Number(instrumentToken)); // Unsubscribe from previously subscribed instrument tokens
@@ -126,7 +166,7 @@ wss.on('connection', (ws) => {
         });
         ticker.on("ticks", onTicks);
       };
-  
+
       a();
     } catch (error) {
       console.error('Error parsing JSON:', error);
@@ -136,6 +176,6 @@ wss.on('connection', (ws) => {
 
 server.listen(7000, () => {
   console.log('Server started on port 7000');
-}); 
+});
 
 module.exports = router;
